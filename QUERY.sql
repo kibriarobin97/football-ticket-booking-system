92d08cd4564ef43be02d5a03ab6fcdfa3cbba085
Golam Kibria <kibriarobin97@gmail.com>
2026-06-14 03:19:00 +0600

    added 2nd query

--- QUERY.sql
+++ QUERY.sql
@@ -121,3 +121,14 @@ FROM
 WHERE
   (tournament_category = 'Champions League')
   AND (match_status = 'Available');
+
+-- Query-2
+SELECT
+  user_id,
+  full_name,
+  email
+FROM
+  users
+WHERE
+  full_name ILIKE 'Tanvir%'
+  OR full_name ILIKE '%Haque%';
